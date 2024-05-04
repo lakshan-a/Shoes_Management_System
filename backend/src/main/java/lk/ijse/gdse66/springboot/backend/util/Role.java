6e58caa2ced4f0d8fccdf98cafc8da9a288e7a25
lakshan <lakshanrashmika16302@gmail.com>
2024-05-04 19:33:23 +0530

    updated

--- backend/src/main/java/lk/ijse/gdse66/springboot/backend/util/Role.java
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/util/Role.java
@@ -5,5 +5,17 @@ package lk.ijse.gdse66.springboot.backend.util;
  * @author: lakshanR
  * @date: 5/4/2024
  */
-public class Role {
+public enum Role {
+    ADMIN,USER;
+
+    public static Role fromString(String text) {
+        if (text != null) {
+            for (Role role : Role.values()) {
+                if (text.equalsIgnoreCase(role.name())) {
+                    return role;
+                }
+            }
+        }
+        throw new IllegalArgumentException("No constant with text " + text + " found in Role enum");
+    }
 }
