8d67f8c8c2d3b8929d081c4756e3e15a013be0a7
lakshan <lakshanrashmika16302@gmail.com>
2024-05-04 19:19:39 +0530

    updated

---
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/BackendApplication.java
@@ -0,0 +1,29 @@
+package lk.ijse.gdse66.springboot.backend;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class BackendApplication {
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(BackendApplication.class, args);
+//    }
+
+    public BackendApplication() throws IOException {
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
+
+}
