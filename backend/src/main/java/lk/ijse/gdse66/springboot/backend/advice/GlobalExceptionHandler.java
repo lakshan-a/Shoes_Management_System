a3f06ed922971cf94264787da843371e29a7dfbc
lakshan <lakshanrashmika16302@gmail.com>
2024-05-04 20:44:34 +0530

    updated

--- backend/src/main/java/lk/ijse/gdse66/springboot/backend/advice/GlobalExceptionHandler.java
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/advice/GlobalExceptionHandler.java
@@ -1,9 +1,70 @@
 package lk.ijse.gdse66.springboot.backend.advice;
 
+import lk.ijse.gdse66.springboot.backend.services.exception.DuplicateRecordException;
+import lk.ijse.gdse66.springboot.backend.services.exception.NotFoundException;
+import lk.ijse.gdse66.springboot.backend.services.exception.ServiceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * @version: v0.0.1
  * @author: lakshanR
  * @date: 5/4/2024
  */
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ServiceException.class)//only handler Services Exception
+    public ResponseEntity<Map<String,Object>> handleServicesException(ServiceException exp){
+        Map<String, Object> errorAttribute = null;
+        if (exp instanceof DuplicateRecordException){
+            getCommonErrorAttribute(HttpStatus.CONFLICT);
+        }else if(exp instanceof NotFoundException){
+            errorAttribute =getCommonErrorAttribute(HttpStatus.NOT_FOUND);
+        }else{
+            errorAttribute =getCommonErrorAttribute(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        errorAttribute.put("message",exp.getMessage());
+
+        return new ResponseEntity<>(errorAttribute,HttpStatus.valueOf((Integer) errorAttribute.get("code")));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String,Object> handleDataValidationException(MethodArgumentNotValidException exp){
+        Map<String,Object> errorAttribute = getCommonErrorAttribute(HttpStatus.BAD_REQUEST);
+        ArrayList<Map<String,Object>> errorList = new ArrayList<>();
+
+        for (FieldError fieldError : exp.getFieldErrors()){
+            LinkedHashMap<String, Object> errorMap = new LinkedHashMap<>();
+            errorMap.put("filed",fieldError.getField());
+            errorMap.put("error",fieldError.getDefaultMessage());
+            errorMap.put("rejected",fieldError.getRejectedValue());
+
+            errorList.add(errorMap);
+        }
+        errorAttribute.put("massage","Data validation failed");
+        errorAttribute.put("errors",errorList);
+
+        return errorAttribute;
+    }
+
+    public Map<String,Object> getCommonErrorAttribute(HttpStatus status){
+        LinkedHashMap<String,Object> errorAttributes = new LinkedHashMap<>();
+        errorAttributes.put("code",status.value());
+        errorAttributes.put("status",status);
+
+        return errorAttributes;
+    }
 }
