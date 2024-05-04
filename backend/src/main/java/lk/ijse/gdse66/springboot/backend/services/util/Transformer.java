7503f7ffd22055aa229445eaf2e4ae40672050f1
lakshan <lakshanrashmika16302@gmail.com>
2024-05-04 19:51:07 +0530

    updated

--- backend/src/main/java/lk/ijse/gdse66/springboot/backend/services/util/Transformer.java
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/services/util/Transformer.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Transformer {
+
     @Autowired
     ModelMapper mapper;
 
--- backend/src/main/java/lk/ijse/gdse66/springboot/backend/services/util/UtilMatter.java
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/services/util/UtilMatter.java
@@ -1,9 +1,14 @@
 package lk.ijse.gdse66.springboot.backend.services.util;
 
+import java.util.UUID;
+
 /**
  * @version: v0.0.1
  * @author: lakshanR
  * @date: 5/4/2024
  */
 public class UtilMatter {
+    public static String generateId() {
+        return UUID.randomUUID().toString();
+    }
 }
