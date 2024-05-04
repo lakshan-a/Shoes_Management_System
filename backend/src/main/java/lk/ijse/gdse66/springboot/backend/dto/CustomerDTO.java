5e5e0c4dd2d786126ccf62e2f3323c470426c69b
lakshan <lakshanrashmika16302@gmail.com>
2024-05-04 19:34:45 +0530

    updated

--- backend/src/main/java/lk/ijse/gdse66/springboot/backend/dto/CustomerDTO.java
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/dto/CustomerDTO.java
@@ -1,9 +1,35 @@
 package lk.ijse.gdse66.springboot.backend.dto;
 
+import lk.ijse.gdse66.springboot.backend.services.exception.ServiceException;
+import lk.ijse.gdse66.springboot.backend.util.CustomerLoyaltyLevel;
+import lk.ijse.gdse66.springboot.backend.util.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  * @version: v0.0.1
  * @author: lakshanR
  * @date: 5/4/2024
  */
-public class CustomerDTO {
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDTO implements Serializable {
+
+    private String code;
+    private String name;
+    private String email;
+    private Gender gender;
+    private String contact;
+    private Date dob;
+    private String addressLine1;
+    private String addressLine2;
+    private Date loyaltyDate;
+    private CustomerLoyaltyLevel loyaltyLevel;
+    private Integer loyaltyPoints;
 }
