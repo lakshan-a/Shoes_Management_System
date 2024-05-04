b805837b67e40a1c7fc409e84ee96eddd6a255a1
lakshan <lakshanrashmika16302@gmail.com>
2024-05-04 19:53:58 +0530

    updated

--- backend/src/main/java/lk/ijse/gdse66/springboot/backend/services/exception/NotFoundException.java
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/services/exception/NotFoundException.java
@@ -1,5 +1,10 @@
 package lk.ijse.gdse66.springboot.backend.services.exception;
 
+/**
+ * @version: v0.0.1
+ * @author: lakshanR
+ * @date: 5/4/2024
+ */
 public class NotFoundException extends ServiceException{
     public NotFoundException(String message) {
         super(message);
--- backend/src/main/java/lk/ijse/gdse66/springboot/backend/services/exception/ServiceException.java
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/services/exception/ServiceException.java
@@ -1,5 +1,10 @@
 package lk.ijse.gdse66.springboot.backend.services.exception;
 
+/**
+ * @version: v0.0.1
+ * @author: lakshanR
+ * @date: 5/4/2024
+ */
 public class ServiceException extends RuntimeException{
     public ServiceException(String message) {
         super(message);
