cc5c00d957a14318f9b8160b43a03e47b3f86603
lakshan <lakshanrashmika16302@gmail.com>
2024-05-04 19:37:00 +0530

    updated

--- backend/src/main/java/lk/ijse/gdse66/springboot/backend/entity/Customer.java
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/entity/Customer.java
@@ -1,9 +1,40 @@
 package lk.ijse.gdse66.springboot.backend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lk.ijse.gdse66.springboot.backend.util.CustomerLoyaltyLevel;
+import lk.ijse.gdse66.springboot.backend.util.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
 /**
  * @version: v0.0.1
  * @author: lakshanR
  * @date: 5/4/2024
  */
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
+
+    @Id
+    private String code;
+    private String name;
+    private String email;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private String contact;
+    private Date dob;
+    private String addressLine1;
+    private String addressLine2;
+    private Date loyaltyDate;
+    private CustomerLoyaltyLevel loyaltyLevel;
+    private Integer loyaltyPoints;
 }
