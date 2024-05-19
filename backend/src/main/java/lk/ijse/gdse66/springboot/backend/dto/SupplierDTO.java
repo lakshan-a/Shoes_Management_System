50fc149130b75c264e9278d9ce98d6e71264bbcf
lakshan <lakshanrashmika16302@gmail.com>
2024-05-19 21:46:05 +0530

    updated

---
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/dto/SupplierDTO.java
@@ -0,0 +1,35 @@
+package lk.ijse.gdse66.springboot.backend.dto;
+
+import lk.ijse.gdse66.springboot.backend.util.SuplierCategory;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 5/5/2024
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SupplierDTO implements Serializable {
+
+    private String code;
+    private String name;
+    private SuplierCategory category;
+    private String addressLine1;
+    private String addressLine2;
+    private String addressLine3;
+    private String addressLine4;
+    private String addressLine5;
+    private String contact_1;
+    private String contact_2;
+    private String email;
+
+
+
+}
