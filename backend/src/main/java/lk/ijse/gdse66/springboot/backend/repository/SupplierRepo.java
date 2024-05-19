f0e7ef29a51cae3db007db3318f59dfbf658ee35
lakshan <lakshanrashmika16302@gmail.com>
2024-05-19 21:48:33 +0530

    updated

---
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/repository/SupplierRepo.java
@@ -0,0 +1,22 @@
+package lk.ijse.gdse66.springboot.backend.repository;
+
+import lk.ijse.gdse66.springboot.backend.entity.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 5/5/2024
+ */
+
+@Repository
+public interface SupplierRepo extends JpaRepository<Supplier,String> {
+
+
+    Supplier findTopByOrderByCodeDesc();
+
+    List<Supplier> findByNameStartingWith(String name);
+}
---
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/services/impl/SupplierServiceImpl.java
@@ -0,0 +1,92 @@
+package lk.ijse.gdse66.springboot.backend.services.impl;
+
+import lk.ijse.gdse66.springboot.backend.dto.SupplierDTO;
+import lk.ijse.gdse66.springboot.backend.entity.Supplier;
+import lk.ijse.gdse66.springboot.backend.repository.SupplierRepo;
+import lk.ijse.gdse66.springboot.backend.services.SupplierService;
+import lk.ijse.gdse66.springboot.backend.services.exception.DuplicateRecordException;
+import lk.ijse.gdse66.springboot.backend.services.exception.NotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 5/5/2024
+ */
+
+@Service
+@Transactional
+public class SupplierServiceImpl implements SupplierService {
+
+    @Autowired
+    private SupplierRepo supplierRepo;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Override
+    public SupplierDTO saveSupplier(SupplierDTO supplierDTO) {
+
+        if (supplierRepo.existsById(supplierDTO.getCode())){
+            throw new DuplicateRecordException("Customer Id is already exists !!");
+        }
+        return mapper.map(supplierRepo.save(mapper.map(supplierDTO, Supplier.class)), SupplierDTO.class);
+    }
+
+    @Override
+    public SupplierDTO updateSupplier(SupplierDTO supplierDTO) {
+
+        if (!supplierRepo.existsById(supplierDTO.getCode())){
+            throw new NotFoundException("Can't find customer id !!");
+        }
+        return mapper.map(supplierRepo.save(mapper.map(supplierDTO, Supplier.class)), SupplierDTO.class);
+    }
+
+    @Override
+    public boolean deleteSupplier(String id) {
+        if (!supplierRepo.existsById(id)){
+            throw new NotFoundException("Can't find customer id !!");
+        }
+        return false;
+    }
+
+    @Override
+    public List<SupplierDTO> getAllSuppliers() {
+        return supplierRepo.findAll().stream().map(supplier -> mapper.map(supplier, SupplierDTO.class)).toList();
+    }
+
+    @Override
+    public List<SupplierDTO> searchSupplier(String name) {
+        return supplierRepo.findByNameStartingWith(name).stream().map(supplier -> mapper.map(supplier, SupplierDTO.class)).toList();
+
+    }
+
+    @Override
+    public String generateNextId() {
+        String prefix = "C";
+        String id = "";
+
+        Supplier lastSupplier = supplierRepo.findTopByOrderByCodeDesc();
+        int nextNumericPart;
+        if (lastSupplier != null) {
+            String lastCode = lastSupplier.getCode();
+            String numericPartString = lastCode.substring(prefix.length());
+            try {
+                int numericPart = Integer.parseInt(numericPartString);
+                nextNumericPart = numericPart + 1;
+            } catch (NumberFormatException e) {
+                nextNumericPart = 1;
+            }
+        } else {
+            nextNumericPart = 1;
+        }
+        id = prefix + String.format("%03d", nextNumericPart);
+
+        return id;
+    }
+}
