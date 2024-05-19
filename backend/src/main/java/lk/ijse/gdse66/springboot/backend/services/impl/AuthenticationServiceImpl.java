5c86ee116d3a03027f41a4f01c5e057b9398c6cb
lakshan <lakshanrashmika16302@gmail.com>
2024-05-19 22:59:42 +0530

    updated

---
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/services/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,55 @@
+package lk.ijse.gdse66.springboot.backend.services.impl;
+
+
+import lk.ijse.gdse66.springboot.backend.auth.Request.SignInRequest;
+import lk.ijse.gdse66.springboot.backend.auth.Request.SignUpRequest;
+import lk.ijse.gdse66.springboot.backend.auth.Response.JWTAuthResponse;
+import lk.ijse.gdse66.springboot.backend.dto.UserDTO;
+import lk.ijse.gdse66.springboot.backend.entity.User;
+import lk.ijse.gdse66.springboot.backend.repository.SecurityRepository;
+import lk.ijse.gdse66.springboot.backend.services.AuthenticationService;
+import lk.ijse.gdse66.springboot.backend.services.JWTService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * @version: v0.0.1
+ * @author: lakshanR
+ * @date: 5/19/2024
+ */
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private final PasswordEncoder passwordEncoder;
+    private final SecurityRepository securityRepository;
+    private final ModelMapper mapper;
+    private final JWTService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public JWTAuthResponse signIn(SignInRequest signInRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(signInRequest.getEmail(), signInRequest.getPassword()));
+        User user =securityRepository.findByEmail(signInRequest.getEmail()).orElseThrow(() -> new UsernameNotFoundException("user not found"));
+        String generatedToken = jwtService.generateToken(user);
+        return JWTAuthResponse.builder().token(generatedToken).build();
+    }
+
+    @Override
+    public JWTAuthResponse signUp(SignUpRequest signUpRequest) {
+        UserDTO userDTO = UserDTO.builder()
+                .email(signUpRequest.getEmail())
+                .password(passwordEncoder.encode(signUpRequest.getPassword()))
+                .role(signUpRequest.getRole())
+                .build();
+        User savedUser = securityRepository.save(mapper.map(userDTO, User.class));
+        String generatedToken = jwtService.generateToken(savedUser);
+        return JWTAuthResponse.builder().token(generatedToken).build();
+    }
+}
