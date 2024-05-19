7da7a8085a705a9a4fe9776544fb2e2afe79ff36
lakshan <lakshanrashmika16302@gmail.com>
2024-05-19 22:54:36 +0530

    updated

---
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/api/UserController.java
@@ -0,0 +1,64 @@
+package lk.ijse.gdse66.springboot.backend.api;
+
+import jakarta.validation.Valid;
+import lk.ijse.gdse66.springboot.backend.dto.UserDTO;
+import lk.ijse.gdse66.springboot.backend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @version: v0.0.1
+ * @author: lakshanR
+ * @date: 5/19/2024
+ */
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    List<UserDTO> getAllCustomer(){
+        return userService.getAllUser();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    UserDTO saveUser(@Valid @RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    void updateCustomer(@Valid @RequestBody UserDTO userDTO){
+        userService.updateUser(userDTO.getEmail(),userDTO);
+    }
+
+    @DeleteMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    void deleteCustomer(@RequestBody UserDTO userDTO){
+        userService.deleteUser(userDTO.getEmail());
+    }
+
+    @PatchMapping(value = "/{email}/{role}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    UserDTO getCustomer(@PathVariable("email") String email, @PathVariable("role") String role){
+        return userService.getUserDetails(email ,role);
+    }
+
+
+
+
+}
