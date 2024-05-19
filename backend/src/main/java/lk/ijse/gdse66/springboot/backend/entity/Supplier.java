ea325d4e17fa220e4305041e37a94a0a0da82289
lakshan <lakshanrashmika16302@gmail.com>
2024-05-19 21:46:37 +0530

    updated

---
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/entity/Supplier.java
@@ -0,0 +1,37 @@
+package lk.ijse.gdse66.springboot.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lk.ijse.gdse66.springboot.backend.util.SuplierCategory;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 5/5/2024
+ */
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Supplier {
+
+    @Id
+    private String code;
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private SuplierCategory category;
+    private String addressLine1;
+    private String addressLine2;
+    private String addressLine3;
+    private String addressLine4;
+    private String addressLine5;
+    private String contact_1;
+    private String contact_2;
+    private String email;
+}
