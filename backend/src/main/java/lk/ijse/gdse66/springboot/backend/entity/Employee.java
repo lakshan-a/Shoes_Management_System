1126f94f1555dd6fdacd5d03e1d095895fa8c0f0
lakshan <lakshanrashmika16302@gmail.com>
2024-05-19 22:15:57 +0530

    updated

---
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/entity/Employee.java
@@ -0,0 +1,80 @@
+package lk.ijse.gdse66.springboot.backend.entity;
+
+import jakarta.persistence.*;
+import lk.ijse.gdse66.springboot.backend.util.Gender;
+import lk.ijse.gdse66.springboot.backend.util.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @version: v0.0.1
+ * @author: lakshanR
+ * @date: 5/6/2024
+ */
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @Column(name = "employee_code", unique = true, nullable = false)
+    private String employeeCode;
+
+    @Column(name = "employee_name", nullable = false)
+    private String employeeName;
+
+    @Column(name = "employee_profile_pic" , columnDefinition = "LONGTEXT")
+    private String employeeProfilePic;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender", nullable = false)
+    private Gender gender;
+
+    @Column(name = "status", nullable = false)
+    private String status;
+
+    @Column(name = "designation", nullable = false)
+    private String designation;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "access_role")
+    private Role accessRole;
+
+    @Column(name = "dob")
+    private Date dob;
+
+    @Column(name = "date_of_join")
+    private Date dateOfJoin;
+
+    @Column(name = "attached_branch")
+    private String attachedBranch;
+
+    @Column(name = "address_line_01", nullable = false)
+    private String addressLine01;
+
+    @Column(name = "address_line_02", nullable = false)
+    private String addressLine02;
+
+    @Column(name = "address_line_03")
+    private String addressLine03;
+
+    @Column(name = "address_line_04")
+    private String addressLine04;
+
+    @Column(name = "address_line_05")
+    private String addressLine05;
+
+    @Column(name = "contact_no", nullable = false)
+    private String contactNo;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "emergency_contact_person")
+    private String emergencyContactPerson;
+}
