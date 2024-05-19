591d6d1148b6c3b9ea5956af9c3a560528f8af2c
lakshan <lakshanrashmika16302@gmail.com>
2024-05-19 23:06:07 +0530

    updated

---
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/auth/Request/SignInRequest.java
@@ -0,0 +1,25 @@
+package lk.ijse.gdse66.springboot.backend.auth.Request;
+
+import lk.ijse.gdse66.springboot.backend.util.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @version: v0.0.1
+ * @author: lakshanR
+ * @date: 5/19/2024
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SignInRequest {
+
+    private String email;
+    private String password;
+    private Role role;
+}
+
