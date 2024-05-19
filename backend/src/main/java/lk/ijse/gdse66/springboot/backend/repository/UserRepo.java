a41346f767756f2bd1e8575d422b1d8ec6f582fd
lakshan <lakshanrashmika16302@gmail.com>
2024-05-19 22:55:45 +0530

    updated

---
+++ backend/src/main/java/lk/ijse/gdse66/springboot/backend/repository/UserRepo.java
@@ -0,0 +1,20 @@
+package lk.ijse.gdse66.springboot.backend.repository;
+
+import lk.ijse.gdse66.springboot.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * @version: v0.0.1
+ * @author: lakshan
+ * @date: 5/19/2024
+ */
+public interface UserRepo extends JpaRepository<User,String> {
+
+    Boolean existsByEmail(String email);
+    User findByEmailAndRole(String email,String role);
+    void deleteByEmail(String email);
+    Optional<User> findByEmail(String email);
+
+}
